2f559862e33e41c7f12353c99e8faa2f5dc63390
Jason Pearson <jason.d.pearson@gmail.com>
2017-02-06 09:19:31 -0500

    initial commit

---
+++ app/src/test/java/io/kaeawc/paths/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package io.kaeawc.paths
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+    @Test
+    fun `addition is correct`() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
